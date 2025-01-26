2c6e13f648b5e0c7bf7d728b189255d6c5c13eee
shisir <shisirhumagain0@gmail.com>
2025-01-25 21:17:14 -0600

    entity role added and db properties added

--- src/main/java/com/employee/EmplMgmtSys/EmplMgmtSysApplication.java
+++ src/main/java/com/employee/EmplMgmtSys/EmplMgmtSysApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmplMgmtSysApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmplMgmtSysApplication.class, args);
 	}
 
---
+++ src/main/java/com/employee/EmplMgmtSys/entity/Role.java
@@ -0,0 +1,42 @@
+package com.employee.EmplMgmtSys.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<Employee> employees;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
