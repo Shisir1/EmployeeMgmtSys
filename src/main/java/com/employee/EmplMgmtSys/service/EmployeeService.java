d9327f3c306886ca63da020094d4cf4a12cd2140
shisir <shisirhumagain0@gmail.com>
2025-01-25 21:04:28 -0600

    service added

--- src/main/java/com/employee/EmplMgmtSys/service/EmployeeService.java
+++ src/main/java/com/employee/EmplMgmtSys/service/EmployeeService.java
@@ -1,4 +1,30 @@
 package com.employee.EmplMgmtSys.service;
 
+import com.employee.EmplMgmtSys.entity.Employee;
+import com.employee.EmplMgmtSys.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployee(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+    public Employee saveEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteById(id);
+    }
 }
